9c799ec3fbbe9567da05b4eb8f745733c40cec60
Loic Rouaud <loic.rouaud@gmail.com>
2022-09-13 22:39:27 +0200

    Lesson sur les dictionnaires

--- 37-dictionnary/dictionnary.py
+++ 37-dictionnary/dictionnary.py
@@ -1,6 +1,29 @@
 dictionnary = {"prenom": "Paul",
 "profession": "ingénieur"}
 
+
+key = dictionnary.get("prenom", "La clé prénom n'existe pas")
+print(key)
+
+# Modifier la valeur associée à une clé
+
+dictionnary["prenom"] = "Julie"
+
+print(dictionnary)
+
+# Ajouter ou supprimer une clé
+dictionnary["age"] = 30
+print(dictionnary)
+
+if "age" in dictionnary:
+  del dictionnary["age"]
+
+# Boucler dans un dictionnaire
 for x in dictionnary.values():
   print(x)
+
+# Obtenir les clés
+dictionnary.keys()
+# Obtenir les valeurs
+dictionnary.values()
 

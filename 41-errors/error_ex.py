96b05e3d65ce39775cfb69508614318080c62088
Loic Rouaud <loic.rouaud@gmail.com>
2022-09-17 00:06:31 +0200

    error exercice

---
+++ 41-errors/error_ex.py
@@ -0,0 +1,12 @@
+fichier = input("Entrez un fichier à ouvrir : ")
+
+try:
+    f = open(fichier, "r")
+    print(f.read())
+except FileNotFoundError:
+    print("Le fichier n'existe pas")
+except UnicodeDecodeError:
+    print("Impossible de lire le fichier")
+else:
+    f.close()
+

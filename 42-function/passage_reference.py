6253019a41ac0d8a52a6d0553ba2feb4367d0eb6
Loic Rouaud <loic.rouaud@gmail.com>
2022-09-18 01:30:51 +0200

    Functions and annotation

--- 42-function/passage_reference.py
+++ 42-function/passage_reference.py
@@ -4,3 +4,58 @@ def foo(param):
 var = 5
 print(id(var))
 foo(param=var)
+
+#---------------------------------
+def foo(param):
+    param = 10
+
+var = 5
+foo(param=var)
+print(var)
+
+#---------------------------------
+def foo(param):
+    param.append(1)
+
+var = []
+foo(param=var)
+print(var)
+
+#---------------------------------
+def foo(param):
+    print(id(param))
+    param.append(1)
+
+var = []
+print(id(var))
+foo(param=var)
+print(var)
+
+#---------------------------------
+def foo(param):
+    param = []
+    param.append(1)
+
+var = []
+foo(param=var)
+print(var)
+
+#---------------------------------
+def foo(param):
+    param = []
+    param.append(4)
+    print(param)
+
+var = [1, 2, 3]
+foo(param=var)
+print(var)
+
+#---------------------------------
+def foo(param):
+    test = param.copy()
+    param.append(4)
+    print(test)
+
+var = [1, 2, 3]
+foo(param=var)
+print(var)
